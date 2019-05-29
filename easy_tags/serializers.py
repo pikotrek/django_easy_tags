e6d5d64536a746d5401136875682505cf0f5cd63
Piotr Karnasiewicz <pikotrek@gmail.com>
2019-05-29 14:12:17 +0200

    Validation

--- easy_tags/serializers.py
+++ easy_tags/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from tagging.models import Tag
 
+ERROR_DICT_PROVIDED = 'error_dict_provided'
+
 
 class TagSerializer(serializers.ModelSerializer):
+    default_error_messages = {
+        ERROR_DICT_PROVIDED: 'Tags should be a list of tag names.'
+    }
+
+    def validate(self, attrs):
+        data = attrs.get('name')
+        try:
+            assert not isinstance(data, dict), ERROR_DICT_PROVIDED
+        except AssertionError as e:
+            self.fail(str(e))
+        return attrs
+
     def to_internal_value(self, data):
         return {'name': data}
 
